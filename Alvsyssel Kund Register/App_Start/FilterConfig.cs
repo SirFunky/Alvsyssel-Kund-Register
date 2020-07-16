f92ed26bd21ed4642d58c4614145eb146a81b2e9
Niklas Karlsson <nika0019@student.hv.se>
2020-07-16 23:50:22 +0200

    Add project files.

---
+++ Alvsyssel Kund Register/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Alvsyssel_Kund_Register
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Alvsyssel Kund Register/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Alvsyssel_Kund_Register.MvcApplication" Language="C#" %>
